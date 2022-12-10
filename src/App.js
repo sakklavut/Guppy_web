ce569657b3cae4193d867f63c3587bc8e8a1f339
sakklavut <sakklavut23395@gmail.com>
2022-12-10 19:51:15 +0700

    demo_1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import TopBar from './components/TopBar';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+        <TopBar />
+        <Navbar />
+    </div>
+  );
+}
+
+export default App;
